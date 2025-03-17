ec37274002b31666694bea58e3d9f6fe77d74847
Sandra Malmtoft <sandra.malmtoft@gmail.com>
2025-03-17 11:07:18 +0100

    content added to script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+let greeting = "Hello, world!";
+
+console.log(greeting);
+
+
+let firstName = "Sandra";
+let age = "27";
+let favoriteColor = "green";
+
+console.log(`Hello! My name is ${firstName}. I'm ${age} years old and my favorite color is ${favoriteColor}.`);
+
+let flowers = 100;
+let plants = 50;
+
+console.log(flowers + plants);
+console.log(flowers - plants);
+console.log(flowers * plants);
+console.log(flowers / plants);
+
+
+
